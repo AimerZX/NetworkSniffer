2650f900d5593548207354ccb1943311daee7b59
Aimer <uxzhao@outlook.com>
2023-03-19 11:12:07 +0800

    改为正式内容

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,8 @@ import time
 from tkinter import ttk
 from scapy.all import sniff
 import threading
+from scapy.layers.inet import *
+from scapy.layers.l2 import *
 
 root = tkinter.Tk()
 root.title("Network Sniffer")
@@ -19,7 +21,6 @@ def start_sniffing():
     stop_button.configure(state="normal")
     current_packet = sniff(count=0, prn=process_packet, stop_filter=(lambda x: stopFlag.is_set()))
 
-  
 
 def stop_sniffing():
     global current_packet, stopFlag
@@ -27,8 +28,6 @@ def stop_sniffing():
     stop_button.configure(state="disabled")
     start_button.configure(state="normal")
     current_packet = None
-    
-    
 
 
 def process_packet(packet):
@@ -36,35 +35,56 @@ def process_packet(packet):
     
     packetstr = packet.show(dump=True)
     packet_counter += 1
-    if packet.haslayer('IP'):
-        row_data = [
-            packet_counter,
-            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(packet.time)),
-            packet['IP'].src,
-            packet['IP'].dst,
-            packet.proto,
-            packet.len,
-            packetstr
-        ]
-        tree.insert("", tkinter.END, values=row_data)
-        tree.update()  
-        root.update()
-    # else:
-    #     row_data = [
-    #         packet_counter,
-    #         time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(packet.time)),
-    #         packet.src,
-    #         packet.dst,
-    #         packet.proto,
-    #         packet.len
-    #     ]
+
+    data_src = packet[Ether].src
+    data_dst = packet[Ether].dst
+    data_type = packet[Ether].type
+    types = {0x0800:'IPv4',0x0806:'ARP',0x86dd:'IPv6',0x88cc:'LLDP',0x891D:'TTE'}
+    if data_type in types:
+        proto = types[data_type]
+    else:
+        proto = 'LOOP'
+    if proto == 'IPv4':
+        protos_ipv4 = {1: 'ICMP', 2: 'IGMP', 4: 'IP', 6: 'TCP', 8: 'EGP', 9: 'IGP', 17: 'UDP', 41: 'IPv6', 50: 'ESP', 89:'OSPF'}
+        data_src = packet[IP].src
+        data_dst = packet[IP].dst
+        proto=packet[IP].proto
+        if proto in protos_ipv4:
+            proto=protos_ipv4[proto]
+
+    if TCP in packet:
+        protos_tcp = {20: 'ftp_data', 21: 'Ftp', 22: 'SSH', 23: 'Telnet', 25: 'SMTP', 80: 'Http', 443: 'Https'}
+        sport = packet[TCP].sport
+        dport = packet[TCP].dport
+        if sport in protos_tcp:
+            proto = protos_tcp[sport]
+        elif dport in protos_tcp:
+            proto = protos_tcp[dport]
+    elif UDP in packet:
+        if packet[UDP].sport == 53 or packet[UDP].dport == 53:
+            proto = 'DNS'
+    
+    row_data = [
+        packet_counter,
+        time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(packet.time)),
+        data_src,
+        data_dst,
+        proto,
+        len(packet),
+        packet.summary(),
+        packetstr
+    ]
+    tree.insert("", tkinter.END, values=row_data)
+    tree.update()  
+    # root.update()
+
 
 def show_packet_info(event):
     global tree, packet_text
     selected_item = tree.selection()[0]
     packet_text.delete("1.0", tkinter.END)
     packet_text.insert(tkinter.END, f"Packet {selected_item}\n")
-    packet_text.insert(tkinter.END, f"{tree.item(selected_item)['values'][6]}")
+    packet_text.insert(tkinter.END, f"{tree.item(selected_item)['values'][7]}")
     
 
 top_frame = tkinter.Frame(root)
@@ -83,10 +103,16 @@ clear_button.pack(side=tkinter.LEFT, padx=5, pady=5)
 middle_frame =  tkinter.Frame(root)
 middle_frame.pack(side= tkinter.TOP, fill= tkinter.BOTH, expand=True)
 
-tree_columns = ("序号", "时间", "源地址", "目标地址", "协议", "长度")
+tree_columns = ("序号", "时间", "源地址", "目标地址", "协议", "长度", "简要信息")
+
 tree = ttk.Treeview(middle_frame, columns=tree_columns, show="headings")
+
+widths =[30,80,80,80,30,30,350]
+
 for col in tree_columns:
     tree.heading(col, text=col)
+for i in range(len(tree_columns)):
+    tree.column(tree_columns[i], width=widths[i])
 
 tree_scrollbar = tkinter.Scrollbar(middle_frame, orient="vertical", command=tree.yview)
 tree_scrollbar.pack(side="right", fill="y")
