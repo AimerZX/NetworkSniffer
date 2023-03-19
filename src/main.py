7e9f07beb8cb1027891fdef77f310c9ff89e57c3
Aimer <uxzhao@outlook.com>
2023-03-19 10:55:34 +0800

    添加滑动条，协议解析，关闭主窗体刷新，放卡，利用线程信号进行嗅探结束控制

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-import tkinter
-
-package=sniff(iface= 'WLAN',count= 1)
-# print(package[0].show())
-root = tkinter.Tk()
-testMsg = tkinter.Message(root, text=str(package[0].show()))
-testMsg.pack()
-root.mainloop()
