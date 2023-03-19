85844935c824b18046ef0f8cc67980f7e03db123
Aimer <uxzhao@outlook.com>
2023-03-19 23:34:38 +0800

    重要更新：协议过滤

--- src/gui.py
+++ src/gui.py
@@ -19,6 +19,15 @@ iface_id =''
 ifaces = get_windows_if_list()
 iface_details = []
 iface_name = []
+
+types = {0x0800:'IPv4',0x0806:'ARP',0x86dd:'IPv6',0x88cc:'LLDP',0x891D:'TTE'}
+protos_tcp = {20: 'ftp_data', 21: 'Ftp', 22: 'SSH', 23: 'Telnet', 25: 'SMTP', 80: 'Http', 443: 'Https'}
+protos_ipv4 = {1: 'ICMP', 2: 'IGMP', 4: 'IP', 6: 'TCP', 8: 'EGP', 9: 'IGP', 17: 'UDP', 41: 'IPv6', 50: 'ESP', 89:'OSPF'}
+
+filter_list = []
+filter_flag = False
+filter_porto = ''
+filter_add_flag = True
 for iface in ifaces:
     iface_details.append((iface['description']))
     iface_name.append(iface['name'])
@@ -55,34 +64,44 @@ def stop_sniffing():
     current_packet = None
 
 def process_packet(packet):
-    global packet_counter, tree
-    
+    global packet_counter, tree, packet_list, filter_flag
+    packet_flag = True
+    if filter_flag:
+        packet_flag = False
+
     packetstr = packet.show(dump=True)
     packet_counter += 1
-
+    packet_list.append(packet)
     data_src = packet[Ether].src
     data_dst = packet[Ether].dst
     data_type = packet[Ether].type
-    types = {0x0800:'IPv4',0x0806:'ARP',0x86dd:'IPv6',0x88cc:'LLDP',0x891D:'TTE'}
+
     if data_type in types:
         proto = types[data_type]
     else:
         proto = 'LOOP'
+    if proto == filter_porto: ##filter
+        packet_flag = True
+
     if proto == 'IPv4':
-        protos_ipv4 = {1: 'ICMP', 2: 'IGMP', 4: 'IP', 6: 'TCP', 8: 'EGP', 9: 'IGP', 17: 'UDP', 41: 'IPv6', 50: 'ESP', 89:'OSPF'}
         data_src = packet[IP].src
         data_dst = packet[IP].dst
         proto=packet[IP].proto
         if proto in protos_ipv4:
             proto=protos_ipv4[proto]
-    if TCP in packet:
-        protos_tcp = {20: 'ftp_data', 21: 'Ftp', 22: 'SSH', 23: 'Telnet', 25: 'SMTP', 80: 'Http', 443: 'Https'}
+        if proto == filter_porto:##filter
+            packet_flag = True
+
+    if TCP in packet:     
         sport = packet[TCP].sport
         dport = packet[TCP].dport
         if sport in protos_tcp:
             proto = protos_tcp[sport]
         elif dport in protos_tcp:
             proto = protos_tcp[dport]
+        if proto == filter_porto:##filter
+            packet_flag = True
+
     elif UDP in packet:
         if packet[UDP].sport == 53 or packet[UDP].dport == 53:
             proto = 'DNS'
@@ -97,9 +116,10 @@ def process_packet(packet):
         packet.summary(),
         packetstr
     ]
-    tree.insert("", tkinter.END, values=row_data)
-    tree.update()  
-    tree.yview_moveto(1)
+    if packet_flag:
+        tree.insert("", tkinter.END, values=row_data)
+        tree.update()  
+        tree.yview_moveto(1)
     # root.update()
 
 def button_clear():
@@ -107,6 +127,12 @@ def button_clear():
     tree.delete(*tree.get_children())
     packet_text.delete("1.0", tkinter.END)
     packet_counter = 0
+    # packet_list=[]
+
+def button_clearall():
+    global packet_list
+    button_clear()
+    packet_list=[]
 
 def show_packet_info(event):
     global tree, packet_text
@@ -121,6 +147,18 @@ def on_select(event):
     iface_des=combobox.get()
     iface_id=iface_name[iface_details.index(iface_des)]
 
+def pro_select(event):
+    global filter_flag, filter_list, packet_list, filter_porto
+    filter_porto=pro_combobox.get()
+    if filter_porto == 'Any':
+        filter_flag = False
+    else:
+        filter_flag = True
+    button_clear()
+    filter_list = packet_list.copy()
+    for packet in filter_list:
+        process_packet(packet)
+    packet_list = filter_list.copy()
 
 #########GUI
 # ttk.Style().theme_use('clam')
@@ -130,11 +168,20 @@ top_frame.pack(side=tkinter.TOP, fill=tkinter.X)
 
 iface_label = ttk.Label(top_frame, text="网卡列表",style='primary.TLabel')
 brf_label = ttk.Label(top_frame, text="BRF规则",style='primary.TLabel')
+pro_label = ttk.Label(top_frame, text="协议过滤",style='primary.TLabel')
 
 combobox = ttk.Combobox(top_frame, values=iface_details,width=30)
 combobox.bind("<<ComboboxSelected>>", on_select)
 combobox.current(0)
 on_select(None)
+
+proList = ['Any', 'IPv4', 'IPv6', 'ftp_data', 'Ftp', 'SSH', 'Telnet', 'SMTP', 'Http', 
+           'Https', 'ICMP', 'IGMP', 'IP', 'TCP', 'EGP', 'IGP', 'UDP', 'IPv6', 'ESP', 'OSPF']
+
+pro_combobox = ttk.Combobox(top_frame, values=proList,width=10)
+pro_combobox.bind("<<ComboboxSelected>>", pro_select)
+pro_combobox.current(0)
+
 
 start_button = tkinter.Button(top_frame, text="开始抓包", command=start_sniffing)
 
@@ -142,16 +189,20 @@ stop_button = tkinter.Button(top_frame, text="停止抓包", command=stop_sniffi
 
 clear_button = tkinter.Button(top_frame, text="清除列表",command=button_clear)
 
-brf_entry = tkinter.Entry(top_frame,width=80 )
+clearall_button = tkinter.Button(top_frame, text="清除缓存",command=button_clearall)
+
+brf_entry = tkinter.Entry(top_frame, width=50)
 
 brf_label.pack(side=tkinter.LEFT, padx=5, pady=0)
 brf_entry.pack(side=tkinter.LEFT, padx=0, pady=5)
 start_button.pack(side=tkinter.LEFT, padx=5, pady=5)
 stop_button.pack(side=tkinter.LEFT, padx=5, pady=5)
 clear_button.pack(side=tkinter.LEFT, padx=5, pady=5)
+clearall_button.pack(side=tkinter.LEFT, padx=5, pady=5)
 iface_label.pack(side=tkinter.LEFT, padx=5, pady=0)
 combobox.pack(side=tkinter.LEFT, padx=0, pady=5)
-
+pro_label.pack(side=tkinter.LEFT, padx=5, pady=0)
+pro_combobox.pack(side=tkinter.LEFT, padx=0, pady=5)
 #########数据包网格
 
 middle_frame =  tkinter.Frame(root)
